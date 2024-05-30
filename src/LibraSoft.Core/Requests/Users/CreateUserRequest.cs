56b1fcc37e84515815e5a4b7a1648c8b728a0ac6
Tarcisio <tarcisioandrade016@gmail.com>
2024-05-30 15:58:54 -0300

    refactor: remove privates sets

--- src/LibraSoft.Core/Requests/Users/CreateUserRequest.cs
+++ src/LibraSoft.Core/Requests/Users/CreateUserRequest.cs
@@ -5,12 +5,12 @@ namespace LibraSoft.Core.Requests.Users
 {
     public class CreateUserRequest
     {
-        public string Name { get; private set; } = string.Empty;
-        public string Email { get; private set; } = string.Empty;
-        public string Telephone { get; private set; } = string.Empty;
-        public Address? Address { get; private set; }
-        public string Password { get; private set; } = string.Empty;
-        public EUserRole Role { get; private set; } = EUserRole.Common;
-        public EUserStatus Status { get; private set; } = EUserStatus.Active;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public Address? Address { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public EUserRole Role { get; set; } = EUserRole.Common;
+        public EUserStatus Status { get; set; } = EUserStatus.Active;
     }
 }
--- src/LibraSoft.Core/ValueObjects/Address.cs
+++ src/LibraSoft.Core/ValueObjects/Address.cs
@@ -2,10 +2,10 @@
 {
     public class Address
     {
-        public string Street { get; private set; } = string.Empty;
-        public string City { get; private set; } = string.Empty;
-        public string State { get; private set; } = string.Empty;
-        public string Country { get; private set; } = string.Empty;
-        public string ZipCode { get; private set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
     }
 }
