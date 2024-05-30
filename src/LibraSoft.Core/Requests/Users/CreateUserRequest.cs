11cab337baa08f83332a5f618e0fde2783d3a6bd
Tarcisio <tarcisioandrade016@gmail.com>
2024-05-30 12:45:18 -0300

    feat: create user request class

---
+++ src/LibraSoft.Core/Requests/Users/CreateUserRequest.cs
@@ -0,0 +1,16 @@
+﻿using LibraSoft.Core.Enums;
+using LibraSoft.Domain.ValueObjects;
+
+namespace LibraSoft.Core.Requests.Users
+{
+    public class CreateUserRequest
+    {
+        public string Name { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string Telephone { get; private set; } = string.Empty;
+        public Address? Address { get; private set; }
+        public string Password { get; private set; } = string.Empty;
+        public EUserRole Role { get; private set; } = EUserRole.Common;
+        public EUserStatus Status { get; private set; } = EUserStatus.Active;
+    }
+}
