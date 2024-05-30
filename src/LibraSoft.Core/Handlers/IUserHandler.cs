01bce5578eaaed6c8abc52c77c9b8e036edced5d
Tarcisio <tarcisioandrade016@gmail.com>
2024-05-30 12:44:47 -0300

    feat: user handler interface

---
+++ src/LibraSoft.Core/Handlers/IUserHandler.cs
@@ -0,0 +1,10 @@
+﻿using LibraSoft.Core.Models;
+using LibraSoft.Core.Requests.Users;
+
+namespace LibraSoft.Core.Handlers
+{
+    public interface IUserHandler
+    {
+        Task<User?> CreateAsync(CreateUserRequest request);
+    }
+}
