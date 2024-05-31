a095ac4112914403e1fdffe10ebf504b4ebaea59
Tarcisio <tarcisioandrade016@gmail.com>
2024-05-31 14:18:13 -0300

    feat: model base class

---
+++ src/LibraSoft.Core/Commons/ModelBase.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using LibraSoft.Core.Exceptions;
+
+namespace LibraSoft.Core.Commons
+{
+    public abstract class ModelBase
+    {
+        protected static void ThrowErrorInValidate(ValidationResult validate)
+        {
+            if (!validate.IsValid)
+            {
+                var messages = validate.Errors.Select(error => error.ErrorMessage).ToList();
+
+                throw new ModelValidateError(messages);
+            }
+        }
+
+        protected abstract void Validate();
+    }
+}
