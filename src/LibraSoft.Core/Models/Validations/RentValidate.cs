fbac206fe50c790847662c90905fa80c5f65f336
Tarcisio <tarcisioandrade016@gmail.com>
2024-06-05 17:02:57 -0300

    fix: incorrect validate beetwen rent date an return date

--- src/LibraSoft.Core/Models/Validations/RentValidate.cs
+++ src/LibraSoft.Core/Models/Validations/RentValidate.cs
@@ -9,8 +9,8 @@ namespace LibraSoft.Core.Models.Validations
         {
             RuleFor(rent => rent.UserId).NotEmpty().WithMessage("Rent user id is required.");
             RuleFor(rent => rent.RentDate).NotEmpty().WithMessage("Rent date required.");
-            RuleFor(rent => rent.ReturnDate).NotEmpty().WithMessage("Rent return date is required.")
-                .GreaterThan(rent => rent.RentDate).WithMessage("The return date cannot be less than the rental date");
+            RuleFor(rent => rent.ReturnDate).NotEmpty().WithMessage("Rent return date is required.");
+            RuleFor(rent => rent.RentDate).LessThanOrEqualTo(rent => rent.ReturnDate).WithMessage("The return date cannot be less than the rental date");
             RuleFor(rent => rent.Status).IsInEnum().WithMessage("Rent status must be a valid.");
         }
     }
