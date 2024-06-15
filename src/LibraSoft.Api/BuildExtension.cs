8ae5e603d9b9e8c1b3e9319e2364367f73c8dbda
Tarcisio <tarcisioandrade016@gmail.com>
2024-06-15 11:27:13 -0300

    feat: abstract api configuration code

---
+++ src/LibraSoft.Api/BuildExtension.cs
@@ -0,0 +1,105 @@
+﻿using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+using LibraSoft.Api.Constants;
+using LibraSoft.Api.Database;
+using LibraSoft.Api.Filters;
+using LibraSoft.Api.Handlers;
+using LibraSoft.Api.Services;
+using LibraSoft.Core.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LibraSoft.Api
+{
+    public static class BuildExtension
+    {
+        public static void AddDatabaseContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>();
+        }
+
+        public static void AddConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRouting(options => options.LowercaseUrls = true);
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+        }
+
+        public static void AddFilters(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+        }
+
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<IUserHandler, UserHandler>();
+            builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+            builder.Services.AddTransient<IBookHandler, BookHandler>();
+            builder.Services.AddTransient<IAuthorHandler, AuthorHandler>();
+            builder.Services.AddTransient<IRentHandler, RentHandler>();
+            builder.Services.AddScoped<ITokenClaimsService, TokenClaimService>();
+        }
+
+        public static void AddDocumentation(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(x =>
+            {
+                x.CustomSchemaIds(n => n.FullName);
+            });
+        }
+
+        public static void AddSwagger(this WebApplication app)
+        {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+        }
+
+        public static void AddCrossOrigin(this WebApplicationBuilder builder)
+        {
+            const string CORS_POLICY = "CorsPolicy";
+
+            builder.Services.AddCors(
+                options => options.AddPolicy(
+                    CORS_POLICY,
+                    policy =>
+                    {
+                        policy.AllowAnyHeader();
+                        policy.AllowAnyMethod();
+                    }
+                        
+                ));
+        }
+
+        public static void AddAuthBuilderConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthorizationBuilder()
+                .AddPolicy("common", policy => policy.RequireClaim(ClaimTypes.Role, "Common"))
+                .AddPolicy("admin", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"));
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthorizationConstants.JWT_SECRET_KEY)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+
+        public static void AddAuthAppConfiguration(this WebApplication app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
--- src/LibraSoft.Api/Program.cs
+++ src/LibraSoft.Api/Program.cs
@@ -1,66 +1,23 @@
-using System.Security.Claims;
-using System.Text;
-using System.Text.Json.Serialization;
-using LibraSoft.Api.Constants;
-using LibraSoft.Api.Data;
-using LibraSoft.Api.Filters;
-using LibraSoft.Api.Handlers;
-using LibraSoft.Api.Services;
-using LibraSoft.Core.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using LibraSoft.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var services = builder.Services;
-
-services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-}); ;
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
-services.AddRouting(options => options.LowercaseUrls = true);
-services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
-services.AddDbContext<AppDbContext>();
-services.AddTransient<IUserHandler, UserHandler>();
-services.AddTransient<ICategoryHandler, CategoryHandler>();
-services.AddTransient<IBookHandler, BookHandler>();
-services.AddTransient<IAuthorHandler, AuthorHandler>();
-services.AddTransient<IRentHandler, RentHandler>();
-services.AddScoped<ITokenClaimsService, TokenClaimService>();
-services.AddAuthorization(options =>
-{
-    options.AddPolicy("common", policy => policy.RequireClaim(ClaimTypes.Role, "Common"));
-    options.AddPolicy("admin", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"));
-});
-services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthorizationConstants.JWT_SECRET_KEY)),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+builder.AddDocumentation();
+builder.AddConfiguration();
+builder.AddFilters();
+builder.AddDatabaseContext();
+builder.AddServices();
+builder.AddAuthBuilderConfiguration();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.AddSwagger();
 }
-app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseHttpsRedirection();
+app.AddAuthAppConfiguration();
 app.MapControllers();
 
 app.Run();
