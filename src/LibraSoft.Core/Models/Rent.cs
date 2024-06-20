46053e5d6d226df5fccbc2b5f309f3cb33a73bfb
Tarcisio <tarcisioandrade016@gmail.com>
2024-06-20 18:22:02 -0300

    feat: new property to indicate if email alert has sended in rent model

--- src/LibraSoft.Core/Models/Rent.cs
+++ src/LibraSoft.Core/Models/Rent.cs
@@ -10,6 +10,7 @@ namespace LibraSoft.Core.Models
         public DateTime RentDate { get; private set; }
         public DateTime ReturnDate { get; private set; }
         public ERentStatus Status { get; private set; }
+        public bool EmailAlertSended { get; private set; } = false;
         public Guid UserId { get; private set; }
         public User User { get; private set; } = null!;
         public IEnumerable<Book> Books { get; private set; } = new List<Book>();
@@ -38,6 +39,21 @@ namespace LibraSoft.Core.Models
         public int BooksRented()
         {
             return Books.Count();
+        }
+
+        public void SetPending()
+        {
+            this.Status = ERentStatus.Pending;
+        }
+
+        public void Return()
+        {
+            this.Status = ERentStatus.Returned;
+        }
+
+        public void EmailAlerted()
+        {
+            this.EmailAlertSended = true;
         }
     }
 }
