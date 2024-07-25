e14b9a67b25e08869056535415b706fab7e74217
Tarcisio <tarcisioandrade016@gmail.com>
2024-07-24 22:48:51 -0300

    feat: bag model

---
+++ src/LibraSoft.Core/Models/Bag.cs
@@ -0,0 +1,21 @@
+﻿using LibraSoft.Core.Commons;
+
+namespace LibraSoft.Core.Models
+{
+    public class Bag : ModelBase
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public Guid BookId { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public Guid UserId { get; private set; }
+        public User User { get; private set; } = null!;
+
+        protected Bag() { }
+
+        public Bag(Guid bookId, Guid userId)
+        {
+            BookId = bookId;
+            UserId = userId;
+        }
+    }
+}
