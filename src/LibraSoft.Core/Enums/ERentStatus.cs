7da3e5852fc8014ae4856a835e36c29b4c0cd3a8
Tarcisio <tarcisioandrade016@gmail.com>
2024-08-02 22:47:58 -0300

    feat: add rent canceled status

--- src/LibraSoft.Core/Enums/ERentStatus.cs
+++ src/LibraSoft.Core/Enums/ERentStatus.cs
@@ -5,6 +5,7 @@
         Requested_Awaiting_Pickup = 1,
         Rent_In_Progress = 2,
         Rent_Expired = 3,
-        Rent_Finished = 4
+        Rent_Finished = 4,
+        Rent_Canceled = 5,
     }
 }
